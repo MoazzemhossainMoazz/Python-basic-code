d07f81927008b360e1d2d118d42ef4bb8f291be6
Moazz <moazzemh222@gmail.com>
2023-06-08 23:56:18 +0600

    Use backslash & variable

---
+++ 3.Use backSlash.py
@@ -0,0 +1,11 @@
+#New line (\n)
+print('Moazzem \nHossain')
+
+#Use double cotation (\")
+print('\"Moazzem Hossain\"')
+
+#Use single cotation (\')
+print('\'Moazzem Hossain\'')
+
+#Use tab
+print('Moazzem \tHossain')
---
+++ 4.Variable.py
@@ -0,0 +1,13 @@
+#Use Variable t-5
+name = "Moazzem Hossain"
+age = 25
+cgpa = 3.89
+print('Hi there, I am ' + name)
+print('I\'m', age, 'years old')
+print('My CGPA is ', cgpa)
+
+print("Moazzem " + "Hossain" + " Moazz")
+firstName = "Moazzem"
+middleName = "Hossain"
+lastName = "Moazz"
+print(firstName + " " + middleName + " " + lastName)
