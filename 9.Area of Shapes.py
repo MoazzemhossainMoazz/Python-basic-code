19e3abb8761ccb3e530c8d2be9af9bbb7ffc40dd
Moazz <moazzemh222@gmail.com>
2023-06-09 23:19:36 +0600

    Area of shapes

---
+++ 9.Area of Shapes.py
@@ -0,0 +1,14 @@
+#Area of any shape t-9
+
+#triangle
+base = float(input("Enter base = "))
+height = float(input("Enter height = "))
+
+area = 0.5 * base * height
+print("Area: ", area)
+
+#circle pi*r*r
+rad = float(input("Enter radius value: "))
+
+circleArea = 3.1416 * rad * rad
+print("Circle Area: ", circleArea)
