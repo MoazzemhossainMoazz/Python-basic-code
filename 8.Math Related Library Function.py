ddeaf167a75c6129ece6869fd8c68d9ea849d0d2
Moazz <moazzemh222@gmail.com>
2023-06-09 23:18:58 +0600

    Math related library function

---
+++ 8.Math Related Library Function.py
@@ -0,0 +1,25 @@
+#Math related library function t-10
+
+from math import *
+result = max(20, 10)
+print(result)
+
+#another system
+print(max(30, 50))
+
+print(min(50, 20))
+print(abs(-4))
+print(pow(2, 3))
+print(sqrt(25))
+print(round(3.8))
+
+
+print(floor(3.7))
+print(ceil(3.7))
+
+#Relational operator & boolean tada type t-12
+print(30 > 20)
+print(30 < 20)
+print(30 >= 20)
+print(30 <= 20)
+print(30 != 20)
