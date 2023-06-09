b40a78cfbc14ebb3a98ab68abc5d7033d6a5c8f9
Moazz <moazzemh222@gmail.com>
2023-06-09 23:18:10 +0600

    type casting

---
+++ 7.Type casting.py
@@ -0,0 +1,11 @@
+#Type casting t-8
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+sum = num1 + num2
+print("The string number: " + sum)
+
+sum = int(num1) + int(num2)
+print("The summation number: ", sum)
+sub = int(num1) - int(num2)
+print("The subtraction number: ", sub)
