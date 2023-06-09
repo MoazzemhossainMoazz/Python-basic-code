fe4dd8608f14527d31fe2abe519ca0c2cf9289c0
Moazz <moazzemh222@gmail.com>
2023-06-09 23:17:25 +0600

    user input

---
+++ 6.User input.py
@@ -0,0 +1,23 @@
+#Getting user input t-7
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gpa = input("Enter your gpa: ")
+
+print("Student Information.")
+print("---------------------")
+print("Name: " + name)
+print("Age: " + age)
+print("gps: " + gpa)
+
+#another way for input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("Summation: ", sum)
+sbu = num1 - num2
+print("Subtraction: ", sub)
+mul = num1 * num2
+print("Multiplication: ", mul)
+div = num1 / num2
+print("Division: ", div)
