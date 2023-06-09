c5aaf74b62bd59e0dbdda279a0d51fed0276605d
Moazz <moazzemh222@gmail.com>
2023-06-09 23:20:09 +0600

    if else statement

---
+++ 10.if,else statement.py
@@ -0,0 +1,27 @@
+#check pass/fail
+
+mark = 20
+
+if mark >= 33:
+    print("pass")
+else:
+    print("fail")
+
+#find largest number
+
+num1 = 20
+num2 = 30
+
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
+
+#even/odd
+
+num = 4
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
