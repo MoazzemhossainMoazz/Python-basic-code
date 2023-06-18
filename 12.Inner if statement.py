66d1bb70804d1d5206f8e77e7a3156673e6349b9
Moazz <moazzemh222@gmail.com>
2023-06-19 00:08:03 +0600

    modify inner if statement

--- 12.Inner if statement.py
+++ 12.Inner if statement.py
@@ -12,3 +12,22 @@ if 7 > 3:
        print("True")
    else:
         print("False")
+
+
+# find greatest number
+
+num1 = 50
+num2 = 30
+num3 = 40
+
+if num1 > num2:
+    if num1 > num3:
+        print("num1")
+    else:
+        print("num3")
+
+if num2 > num1:
+    if num2 > num3:
+        print("num2")
+    else:
+        print("num3")
