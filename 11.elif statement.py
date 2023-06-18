bfbb78aa85237719fc5eab190a968deec5ce5fdd
Moazz <moazzemh222@gmail.com>
2023-06-18 23:51:13 +0600

    added elif statement

---
+++ 11.elif statement.py
@@ -0,0 +1,20 @@
+marks = 20
+
+if marks >= 80:
+    print("A+")
+
+elif marks >= 70:
+    print("A")
+
+elif marks >= 60:
+    print("A-")
+
+elif marks >= 50:
+    print("B")
+
+elif marks >= 40:
+    print("C")
+
+elif marks < 40:
+    print("F")
+
