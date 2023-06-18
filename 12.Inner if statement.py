c0f94952990a9e525da6712a064b185557075cf1
Moazz <moazzemh222@gmail.com>
2023-06-19 00:21:48 +0600

    modify again inner if statement

--- 12.Inner if statement.py
+++ 12.Inner if statement.py
@@ -31,3 +31,18 @@ if num2 > num1:
         print("num2")
     else:
         print("num3")
+
+
+#anoter way
+
+if num1 > num2:
+    if num1 > num3:
+        print("num1")
+    else:
+        print("num3")
+
+else:
+    if num2 > num3:
+        print("num2")
+    else:
+        print("num3")
