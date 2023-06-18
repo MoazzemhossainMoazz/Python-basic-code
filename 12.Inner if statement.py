dcdb93138580c0adb1db3ed4e59106a9fcd6a8a8
Moazz <moazzemh222@gmail.com>
2023-06-18 23:58:01 +0600

    added inner if statement

---
+++ 12.Inner if statement.py
@@ -0,0 +1,14 @@
+# inner same as nasted loop
+
+if 7 > 3:
+   if 7 > 5:
+       print("True")
+   else:
+        print("False")
+
+
+if 7 > 3:
+   if 7 > 9:
+       print("True")
+   else:
+        print("False")
