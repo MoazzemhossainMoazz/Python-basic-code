e88c672a0d56948d46c6c8c7bf77a19a78113303
Moazz <moazzemh222@gmail.com>
2023-06-20 16:26:24 +0600

    letter grade

---
+++ 15.Letter grade.py
@@ -0,0 +1,13 @@
+marks = 75
+
+if marks >= 80 and marks <= 100:
+    print("A+")
+elif marks >= 70 and marks <=80:
+    print("A")
+
+#another way
+
+if 80 <= marks <=100:
+    print("A+")
+elif marks >= 70 and marks <= 79:
+    print("A")
