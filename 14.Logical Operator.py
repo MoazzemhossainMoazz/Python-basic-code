7cecb0e9b332f6ff5e6783852e4a244d71c6a34b
Moazz <moazzemh222@gmail.com>
2023-06-20 16:25:40 +0600

    logical operator

---
+++ 14.Logical Operator.py
@@ -0,0 +1,22 @@
+#find largest value
+
+num1 = 10
+num2 = 50
+num3 = 30
+
+if num1>num2 and num1 > num3:
+    print(num1)
+elif num2>num1 and num2>num3:
+    print(num2)
+else:
+    print(num3)
+
+#find vowel or consonent letter
+print("find Vowel/Consonent")
+
+ch = 'w'
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print("Vowel")
+else:
+    print("Consonent")
