c71f6ace328f6a736b7b9df4e24f29fbc73fc444
Moazz <moazzemh222@gmail.com>
2023-06-21 23:25:40 +0600

    while loop

---
+++ 16.While Loop.py
@@ -0,0 +1,16 @@
+
+i = 1
+while i <= 20:
+    print("Number: ", i)
+    i = i+1
+print("Program end.")
+
+#find even number use while loop
+
+i = 2
+while i <= 20:
+    if i%2 == 0:
+        print(i, "Even")
+    else:
+        print(i, "odd")
+    i = i + 1
