231ef55fb7efc50ed459d03271fec006d6154e95
Moazz <moazzemh222@gmail.com>
2023-06-23 00:04:17 +0600

    list_1

---
+++ 19.List_1.py
@@ -0,0 +1,17 @@
+subjects = ["c", "C++", "java", "python", "Android", "os"]
+
+print(subjects)
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
+
+print("python" in subjects)
+print("HTML" in subjects)
+
+print("CSS" not in subjects)
+print("java" not in subjects)
+
+print(subjects + ["Git"])
+print(subjects + ["php", 8])
+
+print(subjects * 3)
