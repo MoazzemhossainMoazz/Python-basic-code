84d00320a3f947dd68b8bcfd8902b20b65c40ef0
Moazz <moazzemh222@gmail.com>
2023-06-25 00:15:09 +0600

    tuples

---
+++ 30.Tuples.py
@@ -0,0 +1,18 @@
+students = (
+    "Moazzem hossain",
+    "Nayem Islam",
+    "Nazmul Hasan",
+)
+print(students[0])
+
+#another way
+
+students2 = (
+    ("Moazzem Hossain", 3.42, 737921),
+    "Nayem Islam",
+    "Nazmul Hasan"
+)
+print(students2[0])
+print(students2[1:])
+print(students2[0:])
+print(students2[:2])
