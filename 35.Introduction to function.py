a8d26c99ad84b6bc8a06e378ab05fbae4cbd5786
Moazz <moazzemh222@gmail.com>
2023-06-27 00:32:57 +0600

    Introduction to function

---
+++ 35.Introduction to function.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    sum = x + y
+    print(sum)
+
+add(10, 30)
+add(40, 30)
+
+print("\nsummation of 3 numbers")
+def add2(a, b, c):
+    sum2 = a + b + c
+    print(sum2)
+
+add2(10, 30, 40)
+
+print("\nSubtraction")
+def sub(h, k):
+    result = h - k
+    print(result)
+
+sub(50, 30)
+
+def message():
+    print("No parameter")
+message()
