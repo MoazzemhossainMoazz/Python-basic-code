9f3a41c293f662668400fee86f1488e9cec36f25
Moazz <moazzemh222@gmail.com>
2023-07-05 11:08:41 +0600

    write in a file

---
+++ 45.Write in a file/45.Writing in a file.py
@@ -0,0 +1,16 @@
+#Add
+file = open("45.student.txt","a")
+file.write("\nAnd my intake is 34.")
+
+
+
+#create a file then add some text
+
+file2 = open("45.student1.txt", "w")
+
+file.write("\nMy section is 01")
+
+#create html file and add some text
+file = open("45.hello.html", "w")
+file.write("<h1>This is a text</h1>")
+file.close()
