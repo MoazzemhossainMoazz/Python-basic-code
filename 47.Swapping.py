1805dc890d7847b93f6569dd9ab66e03dc147fa3
Moazz <moazzemh222@gmail.com>
2023-07-06 09:01:25 +0600

    swapping

---
+++ 47.Swapping.py
@@ -0,0 +1,26 @@
+a = 30
+b = 40
+
+print("Before Swaping: ")
+print("a = ", a)
+print("b = ", b)
+
+temp = a
+a = b
+b = temp
+
+print("After Swaping: ")
+print("a = ", a)
+print("b = ", b)
+
+#Another way
+print("Another way for swaping: ")
+print("Before Swaping: ")
+print("a = ", a)
+print("b = ", b)
+
+a, b = b, a
+
+print("After Swaping: ")
+print("a = ", a)
+print("b = ", b)
