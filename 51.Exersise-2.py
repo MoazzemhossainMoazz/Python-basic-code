82878212bcb339afd89b7e82742653b871557a79
Moazz <moazzemh222@gmail.com>
2023-07-07 12:19:36 +0600

    Exersise-2

---
+++ 51.Exersise-2.py
@@ -0,0 +1,16 @@
+class Area:
+    l = ""
+    h = ""
+
+    def display(self):
+        print(f"Land: {self.l}, Height: {self.h}")
+
+triangle1 = Area()
+triangle1.l=5
+triangle1.h =6
+triangle1.display()
+
+triangle2 = Area()
+triangle2.l=8
+triangle2.h=9
+triangle2.display()
