8906e2233bb311e6785ecf23cf4252e076aeab6c
Moazz <moazzemh222@gmail.com>
2023-07-07 12:18:59 +0600

    Exersise-1

---
+++ 51.Exersise-1.py
@@ -0,0 +1,13 @@
+class area:
+    l = ""
+    h = ""
+
+triangle1 = area()
+triangle1.l = 5
+triangle1.h = 6
+print(f"land: {triangle1.l}, height: {triangle1.h}")
+
+triangle2 = area()
+triangle2.l = 5
+triangle2.h = 6
+print(f"land: {triangle2.l}, height: {triangle2.h}")
