b5d9ee7669c63e0040dbea36fe68daf1c29c1948
Moazz <moazzemh222@gmail.com>
2023-07-10 22:25:18 +0600

    Module

---
+++ 59.Module/test.py
@@ -0,0 +1,12 @@
+from area import triangle_area,rectangle_area
+
+triangle_area(10, 20)
+rectangle_area(10, 20)
+
+#another way
+
+print("Another way: ")
+from area import *   #used all method in area
+
+triangle_area(40, 50)
+rectangle_area(40, 50)
